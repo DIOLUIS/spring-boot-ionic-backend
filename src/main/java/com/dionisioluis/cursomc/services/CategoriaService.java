77c202b0049e35714e7d6b4dc6a9f1f2cb54647c
DIOLUIS <diogoparazita@gmail.com>
2021-04-14 12:37:19 -0300

    Correções: update em CategoriaService e construtor de pagamento

--- src/main/java/com/dionisioluis/cursomc/services/CategoriaService.java
+++ src/main/java/com/dionisioluis/cursomc/services/CategoriaService.java
@@ -27,15 +27,17 @@ public class CategoriaService {
 				"Objeto não encontrado! Id: " + id
 				+ ", Tipo: " + Categoria.class.getName()));
 		
-			}
+	}
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
 	
+	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);		
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -61,5 +63,8 @@ public class CategoriaService {
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
 
+	}
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
