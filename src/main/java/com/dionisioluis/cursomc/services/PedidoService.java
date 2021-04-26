770e7bdf9e679e135bf52604cb61d2a008df09b8
DIOLUIS <diogoparazita@gmail.com>
2021-04-26 18:16:13 -0300

    Email HTML

--- src/main/java/com/dionisioluis/cursomc/services/PedidoService.java
+++ src/main/java/com/dionisioluis/cursomc/services/PedidoService.java
@@ -69,7 +69,7 @@ public class PedidoService {
 		}
 
 		itemPedidoRepository.saveAll(obj.getItens());
-		emailService.sendOrderConfimationEmail(obj);
+		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 
 	}
