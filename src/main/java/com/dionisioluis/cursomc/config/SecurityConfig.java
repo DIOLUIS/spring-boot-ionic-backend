5862f04d224eaf241210461ccb0f782ee298abbf
DIOLUIS <diogoparazita@gmail.com>
2021-04-27 12:57:38 -0300

    Implementando Altorização

--- src/main/java/com/dionisioluis/cursomc/config/SecurityConfig.java
+++ src/main/java/com/dionisioluis/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.dionisioluis.cursomc.security.JWTAuthenticationFilter;
+import com.dionisioluis.cursomc.security.JWTAuthorizationFilter;
 import com.dionisioluis.cursomc.security.JWTUtil;
 
 @Configuration
@@ -61,6 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 				
 		}
